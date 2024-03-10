0adc916d78871bf9db554045a9a18f6d8b951746
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-03-10 21:38:30 +0200

    download packeges and image

--- spotify/app/page.tsx
+++ spotify/app/page.tsx
@@ -1,7 +1,48 @@
-import Image from "next/image";
+import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
-    <div>hellow</div>
+    <div
+      className="
+          bg-neutral-900
+          rounded-lg
+          h-full
+          w-full
+          overflow-hidden
+          overflow-y-auto"
+    >
+      <Header>
+        <div className="mb-2">
+          <h1 className="text-wite text-3xl font-semibold"> Welcome</h1>
+          <div
+            className="
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          xl:grid-cols-3
+          2xl:grid-cols-4
+          gap-3
+          mt-4
+          "
+          >
+            <ListItem image="/images/image.png" href="#" name="name" />
+          </div>
+        </div>
+      </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div
+          className="
+        flex
+        items-center
+        justify-between
+        "
+        >
+          <h1 className="text-white text-2xl font-semibold"> Newest songs </h1>
+        </div>
+<div className="">List of songs</div>
+
+      </div>
+    </div>
   );
 }
