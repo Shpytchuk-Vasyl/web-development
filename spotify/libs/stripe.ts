de41bf9f120d3731d55c12c4cfa03d6b8eb7c4b8
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-03-16 16:18:14 +0200

    add  subscriptions and stripe

---
+++ spotify/libs/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from "stripe";
+
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+  apiVersion: "2023-10-16",
+  appInfo: {
+    name: "Spotify Clone with Next.js and React",
+    version: "0.1.0",
+  },
+});
